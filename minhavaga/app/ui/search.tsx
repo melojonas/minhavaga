f787b66b6b93541e67a462fa49c2abf7b6c19d1e
Jonas Melo <jonasmelo@ufrj.br>
2024-11-30 20:34:27 +0000

    adiciona configuração inicial do projeto Next.js com Tailwind CSS

---
+++ minhavaga/app/ui/search.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div className="rounded-tl-none rounded-tr-none rounded-bl-none rounded-br-none rotate-0 opacity-100 relative">
+      <div className="w-[520px] h-12 pt-0 pr-0 pb-0 pl-0 rounded-tl-[30px] rounded-tr-[30px] rounded-bl-[30px] rounded-br-[30px] rotate-0 opacity-100 bg-[#ffffff] relative border-t-[1px] border-r-[1px] border-b-[1px] border-l-[1px] border-t-[#1452af] border-r-[#1452af] border-b-[#1452af] border-l-[#1452af]">
+        <div className="w-5 h-5 pt-0 pr-0 pb-0 pl-0 rounded-tl-none rounded-tr-none rounded-bl-none rounded-br-none rotate-0 opacity-100 relative">
+          <div className="rotate-0 opacity-100"></div>
+          <div className="rotate-0 opacity-100"></div>
+        </div>
+      </div>
+      <div className="w-[520px] h-[209px] pt-0 pr-0 pb-0 pl-0 rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[20px] rounded-br-[20px] rotate-0 opacity-100 bg-[#ffffff] relative">
+        <p className="w-[38px] h-5 rotate-0 opacity-100 text-left text-sm font-bold relative text-[#000000] leading-5">Curso</p>
+        <p className="w-[66px] h-5 rotate-0 opacity-100 text-left text-sm font-normal relative text-[#000000] leading-5">Pedagogia</p>
+        <p className="w-[134px] h-5 rotate-0 opacity-100 text-left text-sm font-normal relative text-[#000000] leading-5">Engenharia Eletrônica</p>
+        <p className="w-[42px] h-5 rotate-0 opacity-100 text-left text-sm font-normal relative text-[#000000] leading-5">Direito</p>
+        <p className="w-[86px] h-5 rotate-0 opacity-100 text-left text-sm font-bold relative text-[#000000] leading-5">Universidade</p>
+        <p className="w-[241px] h-5 rotate-0 opacity-100 text-left text-sm font-normal relative text-[#000000] leading-5">Universidade Federal do Rio de Janeiro</p>
+        <p className="w-7 h-5 rotate-0 opacity-100 text-left text-sm font-light relative text-[#000000] leading-5">UFRJ</p>
+        <p className="w-[262px] h-5 rotate-0 opacity-100 text-left text-sm font-normal relative text-[#000000] leading-5">Universidade Federal da Grande Dourados</p>
+        <p className="w-[35px] h-5 rotate-0 opacity-100 text-left text-sm font-light relative text-[#000000] leading-5">UFGD</p>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
